9402d4857f8dba47e0d6ce96955e054e6790a1d6
dylanleclair <dylan.leclair@icloud.com>
2021-04-12 19:36:03 -0600

    Changed database to SQLite

--- realbeast/mysite/mysite/settings.py
+++ realbeast/mysite/mysite/settings.py
@@ -79,14 +79,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'realbeast',
-        'USER':'root',
-        'PASSWORD':'password',
-        'HOST':'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
